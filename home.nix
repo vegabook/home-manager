a119cf128c1c682dc1c3e97d5c7849fd236a4cb8
vegabook <thomas.browne@mac.com>
2023-12-04 05:29:20 +0000

    alacritty colours

--- home.nix
+++ home.nix
@@ -182,6 +182,10 @@ in
       source = ./alacritty;
       recursive = true;
     };  
+    "./.config/helix" = {
+      source = ./helix;
+      recursive = true;
+    };  
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
