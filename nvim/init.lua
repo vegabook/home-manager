065308115c5ff7c0fc1a0e19f3a9ba10bb4d52d0
vegabook <thomas.browne@mac.com>
2024-02-21 00:14:51 +0000

    fz for fuz

--- nvim/init.lua
+++ nvim/init.lua
@@ -112,7 +112,7 @@ vim.keymap.set('n', '<Left>', ':tabp<cr>')
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fz', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
