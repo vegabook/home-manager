d05397463963aee71744a1d04926e2ef26745491
vegabook <thomas@scendance.fr>
2024-05-28 19:00:32 +0100

    rainbow csv nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,6 +28,25 @@ local plugins = {
   'nvim-tree/nvim-web-devicons',
   'mlochbaum/BQN',
   {
+    'cameron-wags/rainbow_csv.nvim',
+    config = true,
+    ft = {
+        'csv',
+        'tsv',
+        'csv_semicolon',
+        'csv_whitespace',
+        'csv_pipe',
+        'rfc_csv',
+        'rfc_semicolon'
+    },
+    cmd = {
+        'RainbowDelim',
+        'RainbowDelimSimple',
+        'RainbowDelimQuoted',
+        'RainbowMultiDelim'
+    }
+  },
+  {
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
       config = function () 
