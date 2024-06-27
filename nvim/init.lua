e3483b2e47d50940b009738caf38f683a3e805e9
vegabook <thomas@scendance.fr>
2024-06-27 22:30:40 +0100

    better elixir syntax

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,7 +90,7 @@ local plugins = {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "python", "c", "lua", "vim", "vimdoc", "query", "elixir", "erlang", "heex", "javascript", "html" },
+            ensure_installed = { "python", "c", "lua", "vim", "vimdoc", "query", "erlang", "heex", "javascript", "html" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },  
