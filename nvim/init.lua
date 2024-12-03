0d067e3b8f6ca515b5f4611bdd89229b49172936
vegabook <thomas@scendance.fr>
2024-12-03 21:31:04 +0000

    whatever

--- nvim/init.lua
+++ nvim/init.lua
@@ -114,6 +114,13 @@ require("nvim-tree").setup({
   renderer = {
     group_empty = true,
   },
+  git = {
+    enable = true,
+  },
+  filters = {
+    dotfiles = true,
+    git_ignored = true,
+  },
 })
 
 -- Global settings
