c31d4929dcb3d412e69f1c6302a2723b2f569478
vegabook <thomas.browne@mac.com>
2024-12-16 23:57:16 +0000

    home manager update

--- home.nix
+++ home.nix
@@ -59,19 +59,13 @@ in
     rclone
     tree
     ack
-    iosevka
     bqn386
     chafa # sixel viewer
     fuse
     neovim
     nodejs
-    (nerdfonts.override { fonts = [ "IosevkaTermSlab" "Inconsolata" "FantasqueSansMono" "3270" "HeavyData"]; })
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    nerd-fonts.fira-code
+    nerd-fonts.iosevka
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
