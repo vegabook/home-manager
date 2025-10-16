121e2927535bcdce3d00200efe7ec1ee82da414b
vegabook <thomas@scendance.fr>
2025-10-16 16:00:27 +0100

    with iosevka

--- home.nix
+++ home.nix
@@ -67,6 +67,8 @@ in
     iosevka
     asciinema
     bqn386
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.iosevka
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
