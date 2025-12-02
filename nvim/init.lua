c7d95f0c81446b8ab20f0aaac886b67188d016c8
vegabook <thomas@scendance.fr>
2025-12-02 22:31:50 +0000

    no more flakes for languages

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,6 +34,8 @@ local plugins = {
   'rockerBOO/boo-colorscheme-nvim',
   'mlochbaum/BQN',
   'slugbyte/lackluster.nvim',
+  'water-sucks/darkrose.nvim',
+
   {
     'cameron-wags/rainbow_csv.nvim',
     config = true,
@@ -61,13 +63,13 @@ local plugins = {
 
         configs.setup({
             ensure_installed = { "python", "c", "lua", "vim", "vimdoc", 
-              "query", "erlang", "heex", "eex", "elixir", "javascript", "html", "r"},
+              "query", "erlang", "heex", "eex", "elixir", "javascript", "html", "r", "zig" },
             sync_install = false,
-            highlight = { enable = true, 
-              additional_vim_regex_highlighting = { "elixir" }, 
+            highlight = { 
+              enable = true, 
             },
-            indent = { enable = true,
-            disable = { "elixir"}, 
+            indent = { 
+              enable = true,
             },  
           })
       end
@@ -92,84 +94,6 @@ local plugins = {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' }
   },
-
-  {
-    "folke/sidekick.nvim",
-    opts = {
-      -- add any options here
-      cli = {
-        mux = {
-          backend = "zellij",
-          enabled = true,
-        },
-      },
-    },
-    keys = {
-      {
-        "<tab>",
-        function()
-          -- if there is a next edit, jump to it, otherwise apply it if any
-          if not require("sidekick").nes_jump_or_apply() then
-            return "<Tab>" -- fallback to normal tab
-          end
-        end,
-        expr = true,
-        desc = "Goto/Apply Next Edit Suggestion",
-      },
-      {
-        "<c-.>",
-        function() require("sidekick.cli").toggle() end,
-        desc = "Sidekick Toggle",
-        mode = { "n", "t", "i", "x" },
-      },
-      {
-        "<leader>aa",
-        function() require("sidekick.cli").toggle() end,
-        desc = "Sidekick Toggle CLI",
-      },
-      {
-        "<leader>as",
-        function() require("sidekick.cli").select() end,
-        -- Or to select only installed tools:
-        -- require("sidekick.cli").select({ filter = { installed = true } })
-        desc = "Select CLI",
-      },
-      {
-        "<leader>ad",
-        function() require("sidekick.cli").close() end,
-        desc = "Detach a CLI Session",
-      },
-      {
-        "<leader>at",
-        function() require("sidekick.cli").send({ msg = "{this}" }) end,
-        mode = { "x", "n" },
-        desc = "Send This",
-      },
-      {
-        "<leader>af",
-        function() require("sidekick.cli").send({ msg = "{file}" }) end,
-        desc = "Send File",
-      },
-      {
-        "<leader>av",
-        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
-        mode = { "x" },
-        desc = "Send Visual Selection",
-      },
-      {
-        "<leader>ap",
-        function() require("sidekick.cli").prompt() end,
-        mode = { "n", "x" },
-        desc = "Sidekick Select Prompt",
-      },
-      -- Example of a keybinding to open Claude directly
-      {
-        "<leader>ac",
-        function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
-        desc = "Sidekick Toggle Claude",
-      },
-    },
-  }
 
 }
 
