a0660765746e892b050e48edb222f69b713f648c
vegabook <thomas.browne@mac.com>
2026-03-05 14:57:35 +0000

    codecompanion

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,6 +38,19 @@ local plugins = {
   "water-sucks/darkrose.nvim",
   "github/copilot.vim",
   {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      -- NOTE: The log_level is in `opts.opts`
+      opts = {
+        log_level = "DEBUG", -- or "TRACE"
+      },
+    },
+  },
+  {
     "hat0uma/csvview.nvim",
     ---@module "csvview"
     ---@type CsvView.Options
