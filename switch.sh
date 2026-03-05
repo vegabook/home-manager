fc05971b01a25a616f6aa9bcae5bb7bbcd3603f0
vegabook <thomas.browne@mac.com>
2026-03-05 14:29:42 +0000

    flake on mac too

--- switch.sh
+++ switch.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 cd ~/.config/home-manager
-home-manager switch --flake .#tbrowne
+
+# Detect platform and use appropriate flake configuration
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  if [[ $(uname -m) == "arm64" ]]; then
+    # Apple Silicon
+    home-manager switch --flake .#tbrowne@aarch64-darwin
+  else
+    # Intel Mac
+    home-manager switch --flake .#tbrowne@x86_64-darwin
+  fi
+else
+  # Linux
+  home-manager switch --flake .#tbrowne
+fi
